6ec4d374b92f64baa414b19d6e9778aeda7d8540
ardalan-di <ardalandelfardi@gmail.com>
2025-06-14 21:04:50 +0330

    Update Create_Flight_Dim.sql

--- Datawarehouse/Dimensions/Create_Flight_Dim.sql
+++ Datawarehouse/Dimensions/Create_Flight_Dim.sql
@@ -1,6 +1,5 @@
 CREATE TABLE [DW].[DimFlight] (
   [FlightKey] int PRIMARY KEY,
-  [FlightNumber] nvarchar(255),
   [DepartureDateTime] datetime,
   [ArrivalDateTime] datetime,
   [FlightDurationMinutes] int
