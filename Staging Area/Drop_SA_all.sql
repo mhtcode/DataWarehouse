62584d86ae39222b09b5d84091b66cd28d521995
mhtcode <masihtanoursaz@gmail.com>
2025-06-26 01:41:17 +0330

    Drop SA tables updated for loyalty mart

--- Staging Area/Drop_SA_all.sql
+++ Staging Area/Drop_SA_all.sql
@@ -1,14 +1,15 @@
--- Drop all staging tables in the SA schema
+-- Drop all staging tables in the SA schema (Complete List)
 -- Order is arranged to drop dependent tables before their parents
 
 DROP TABLE IF EXISTS [SA].[Payment];
 DROP TABLE IF EXISTS [SA].[Reservation];
 DROP TABLE IF EXISTS [SA].[SeatDetail];
+DROP TABLE IF EXISTS [SA].[ServiceOfferingItem];
 DROP TABLE IF EXISTS [SA].[ServiceOffering];
 DROP TABLE IF EXISTS [SA].[TravelClass];
 
-DROP TABLE IF EXISTS [SA].[FlightDetail];
 DROP TABLE IF EXISTS [SA].[FlightOperation];
+DROP TABLE IF EXISTS [SA].[FlightDetail];
 
 DROP TABLE IF EXISTS [SA].[CrewAssignment];
 DROP TABLE IF EXISTS [SA].[CrewMember];
@@ -25,10 +26,16 @@ DROP TABLE IF EXISTS [SA].[Aircraft];
 DROP TABLE IF EXISTS [SA].[Airport];
 DROP TABLE IF EXISTS [SA].[Airline];
 
+DROP TABLE IF EXISTS [SA].[Item];
+DROP TABLE IF EXISTS [SA].[LoyaltyTransactionType];
+DROP TABLE IF EXISTS [SA].[PointConversionRate];
+DROP TABLE IF EXISTS [SA].[LoyaltyTier];
+
 DROP TABLE IF EXISTS [SA].[MaintenanceType];
 DROP TABLE IF EXISTS [SA].[Technician];
 DROP TABLE IF EXISTS [SA].[MaintenanceLocation];
 DROP TABLE IF EXISTS [SA].[Part];
 
 DROP TABLE IF EXISTS [SA].[AirlineAirportService];
-DROP TABLE IF EXISTS [SA].[LoyaltyTier];
+
+DROP SCHEMA IF EXISTS [SA];
