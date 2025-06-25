5ee35804fd81be793a2414cf5e412588c9880f33
mhtcode <masihtanoursaz@gmail.com>
2025-06-26 00:14:53 +0330

    Drop tables updated

--- Source/Drop_Source_all.sql
+++ Source/Drop_Source_all.sql
@@ -7,14 +7,18 @@ DROP TABLE IF EXISTS [Source].[CrewAssignment];
 DROP TABLE IF EXISTS [Source].[FlightOperation];
 DROP TABLE IF EXISTS [Source].[Reservation];
 DROP TABLE IF EXISTS [Source].[SeatDetail];
-DROP TABLE IF EXISTS [Source].[ServiceOffering];  -- Must drop before TravelClass
-DROP TABLE IF EXISTS [Source].[CrewMember];
+DROP TABLE IF EXISTS [Source].[ServiceOfferingItem];
+DROP TABLE IF EXISTS [Source].[ServiceOffering];
+DROP TABLE IF EXISTS [Source].[Item];
+DROP TABLE IF EXISTS [Source].[TravelClass];
+DROP TABLE IF EXISTS [Source].[LoyaltyTransactionType];
 DROP TABLE IF EXISTS [Source].[Account];
+DROP TABLE IF EXISTS [Source].[LoyaltyTier];
 DROP TABLE IF EXISTS [Source].[Passenger];
 DROP TABLE IF EXISTS [Source].[FlightDetail];
 DROP TABLE IF EXISTS [Source].[Aircraft];
-DROP TABLE IF EXISTS [Source].[TravelClass];      -- Now safe to drop
-DROP TABLE IF EXISTS [Source].[LoyaltyTier];
+DROP TABLE IF EXISTS [Source].[CrewMember];
+DROP TABLE IF EXISTS [Source].[PointConversionRate];
 DROP TABLE IF EXISTS [Source].[Person];
 DROP TABLE IF EXISTS [Source].[Airport];
 DROP TABLE IF EXISTS [Source].[Airline];
@@ -23,3 +27,6 @@ DROP TABLE IF EXISTS [Source].[Technician];
 DROP TABLE IF EXISTS [Source].[MaintenanceLocation];
 DROP TABLE IF EXISTS [Source].[Part];
 DROP TABLE IF EXISTS [Source].[AirlineAirportService];
+
+
+DROP SCHEMA IF EXISTS [Source];
