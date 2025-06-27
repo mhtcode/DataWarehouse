7ab19e11f8cebe9f8e65a6ef2af0a99fe5a3bc1a
Pouria Dianati <137393369+Pouriadia@users.noreply.github.com>
2025-06-27 18:13:09 +0330

    Change Drop statements to alphabetical as well

--- Source/Drop_Source_all.sql
+++ Source/Drop_Source_all.sql
@@ -1,32 +1,27 @@
--- Drop tables in reverse dependency order
-DROP TABLE IF EXISTS [Source].[PointsTransaction];
-DROP TABLE IF EXISTS [Source].[Points];
-DROP TABLE IF EXISTS [Source].[AccountTierHistory];
-DROP TABLE IF EXISTS [Source].[Payment];
+DROP TABLE IF EXISTS [Source].[Account];
+DROP TABLE IF EXISTS [Source].[Aircraft];
+DROP TABLE IF EXISTS [Source].[Airline];
+DROP TABLE IF EXISTS [Source].[AirlineAirportService];
+DROP TABLE IF EXISTS [Source].[Airport];
 DROP TABLE IF EXISTS [Source].[CrewAssignment];
+DROP TABLE IF EXISTS [Source].[CrewMember];
+DROP TABLE IF EXISTS [Source].[FlightDetail];
 DROP TABLE IF EXISTS [Source].[FlightOperation];
+DROP TABLE IF EXISTS [Source].[Item];
+DROP TABLE IF EXISTS [Source].[LoyaltyTier];
+DROP TABLE IF EXISTS [Source].[LoyaltyTransactionType];
+DROP TABLE IF EXISTS [Source].[Part];
+DROP TABLE IF EXISTS [Source].[Passenger];
+DROP TABLE IF EXISTS [Source].[Payment];
+DROP TABLE IF EXISTS [Source].[PointConversionRate];
+DROP TABLE IF EXISTS [Source].[Points];
+DROP TABLE IF EXISTS [Source].[PointsTransaction];
 DROP TABLE IF EXISTS [Source].[Reservation];
 DROP TABLE IF EXISTS [Source].[SeatDetail];
-DROP TABLE IF EXISTS [Source].[ServiceOfferingItem];
 DROP TABLE IF EXISTS [Source].[ServiceOffering];
-DROP TABLE IF EXISTS [Source].[Item];
-DROP TABLE IF EXISTS [Source].[TravelClass];
-DROP TABLE IF EXISTS [Source].[LoyaltyTransactionType];
-DROP TABLE IF EXISTS [Source].[Account];
-DROP TABLE IF EXISTS [Source].[LoyaltyTier];
-DROP TABLE IF EXISTS [Source].[Passenger];
-DROP TABLE IF EXISTS [Source].[FlightDetail];
-DROP TABLE IF EXISTS [Source].[Aircraft];
-DROP TABLE IF EXISTS [Source].[CrewMember];
-DROP TABLE IF EXISTS [Source].[PointConversionRate];
-DROP TABLE IF EXISTS [Source].[Person];
-DROP TABLE IF EXISTS [Source].[Airport];
-DROP TABLE IF EXISTS [Source].[Airline];
-DROP TABLE IF EXISTS [Source].[MaintenanceType];
+DROP TABLE IF EXISTS [Source].[ServiceOfferingItem];
 DROP TABLE IF EXISTS [Source].[Technician];
-DROP TABLE IF EXISTS [Source].[MaintenanceLocation];
-DROP TABLE IF EXISTS [Source].[Part];
-DROP TABLE IF EXISTS [Source].[AirlineAirportService];
-
+DROP TABLE IF EXISTS [Source].[TravelClass];
 
 DROP SCHEMA IF EXISTS [Source];
+GO
--- Staging Area/Drop_SA_all.sql
+++ Staging Area/Drop_SA_all.sql
@@ -1,81 +1,59 @@
--- Drop all tables in [SA] schema (dependency order)
-
-DROP TABLE IF EXISTS [SA].[Payment];
-DROP TABLE IF EXISTS [SA].[Reservation];
-DROP TABLE IF EXISTS [SA].[SeatDetail];
-DROP TABLE IF EXISTS [SA].[ServiceOfferingItem];
-DROP TABLE IF EXISTS [SA].[ServiceOffering];
-DROP TABLE IF EXISTS [SA].[TravelClass];
-
-DROP TABLE IF EXISTS [SA].[FlightOperation];
-DROP TABLE IF EXISTS [SA].[FlightDetail];
-
+DROP TABLE IF EXISTS [SA].[Account];
+DROP TABLE IF EXISTS [SA].[AccountTierHistory];
+DROP TABLE IF EXISTS [SA].[Aircraft];
+DROP TABLE IF EXISTS [SA].[Airline];
+DROP TABLE IF EXISTS [SA].[AirlineAirportService];
+DROP TABLE IF EXISTS [SA].[Airport];
 DROP TABLE IF EXISTS [SA].[CrewAssignment];
 DROP TABLE IF EXISTS [SA].[CrewMember];
-
-DROP TABLE IF EXISTS [SA].[AccountTierHistory];
-DROP TABLE IF EXISTS [SA].[PointsTransaction];
-DROP TABLE IF EXISTS [SA].[Points];
-DROP TABLE IF EXISTS [SA].[Account];
-
+DROP TABLE IF EXISTS [SA].[FlightDetail];
+DROP TABLE IF EXISTS [SA].[FlightOperation];
+DROP TABLE IF EXISTS [SA].[Item];
+DROP TABLE IF EXISTS [SA].[LoyaltyTier];
+DROP TABLE IF EXISTS [SA].[LoyaltyTransactionType];
+DROP TABLE IF EXISTS [SA].[MaintenanceLocation];
+DROP TABLE IF EXISTS [SA].[MaintenanceType];
+DROP TABLE IF EXISTS [SA].[Part];
+DROP TABLE IF EXISTS [SA].[Payment];
 DROP TABLE IF EXISTS [SA].[Passenger];
 DROP TABLE IF EXISTS [SA].[Person];
-
-DROP TABLE IF EXISTS [SA].[Aircraft];
-DROP TABLE IF EXISTS [SA].[Airport];
-DROP TABLE IF EXISTS [SA].[Airline];
-
-DROP TABLE IF EXISTS [SA].[Item];
-DROP TABLE IF EXISTS [SA].[LoyaltyTransactionType];
 DROP TABLE IF EXISTS [SA].[PointConversionRate];
-DROP TABLE IF EXISTS [SA].[LoyaltyTier];
-
-DROP TABLE IF EXISTS [SA].[MaintenanceType];
+DROP TABLE IF EXISTS [SA].[Points];
+DROP TABLE IF EXISTS [SA].[PointsTransaction];
+DROP TABLE IF EXISTS [SA].[Reservation];
+DROP TABLE IF EXISTS [SA].[SeatDetail];
+DROP TABLE IF EXISTS [SA].[ServiceOffering];
+DROP TABLE IF EXISTS [SA].[ServiceOfferingItem];
 DROP TABLE IF EXISTS [SA].[Technician];
-DROP TABLE IF EXISTS [SA].[MaintenanceLocation];
-DROP TABLE IF EXISTS [SA].[Part];
+DROP TABLE IF EXISTS [SA].[TravelClass];
 
-DROP TABLE IF EXISTS [SA].[AirlineAirportService];
-
--- Now drop all ETL procedures for these tables
-IF OBJECT_ID('[SA].[ETL_Payment]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_Payment];
-IF OBJECT_ID('[SA].[ETL_Reservation]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_Reservation];
-IF OBJECT_ID('[SA].[ETL_SeatDetail]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_SeatDetail];
-IF OBJECT_ID('[SA].[ETL_ServiceOfferingItem]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_ServiceOfferingItem];
-IF OBJECT_ID('[SA].[ETL_ServiceOffering]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_ServiceOffering];
-IF OBJECT_ID('[SA].[ETL_TravelClass]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_TravelClass];
-
-IF OBJECT_ID('[SA].[ETL_FlightOperation]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_FlightOperation];
-IF OBJECT_ID('[SA].[ETL_FlightDetail]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_FlightDetail];
-
+-- Drop all ETL procedures in alphabetical order
+IF OBJECT_ID('[SA].[ETL_Account]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_Account];
+IF OBJECT_ID('[SA].[ETL_AccountTierHistory]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_AccountTierHistory];
+IF OBJECT_ID('[SA].[ETL_Aircraft]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_Aircraft];
+IF OBJECT_ID('[SA].[ETL_Airline]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_Airline];
+IF OBJECT_ID('[SA].[ETL_AirlineAirportService]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_AirlineAirportService];
+IF OBJECT_ID('[SA].[ETL_Airport]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_Airport];
 IF OBJECT_ID('[SA].[ETL_CrewAssignment]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_CrewAssignment];
 IF OBJECT_ID('[SA].[ETL_CrewMember]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_CrewMember];
-
-IF OBJECT_ID('[SA].[ETL_AccountTierHistory]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_AccountTierHistory];
-IF OBJECT_ID('[SA].[ETL_PointsTransaction]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_PointsTransaction];
-IF OBJECT_ID('[SA].[ETL_Points]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_Points];
-IF OBJECT_ID('[SA].[ETL_Account]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_Account];
-
-IF OBJECT_ID('[SA].[ETL_Passenger]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_Passenger];
-IF OBJECT_ID('[SA].[ETL_Person]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_Person];
-
-IF OBJECT_ID('[SA].[ETL_Aircraft]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_Aircraft];
-IF OBJECT_ID('[SA].[ETL_Airport]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_Airport];
-IF OBJECT_ID('[SA].[ETL_Airline]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_Airline];
-
+IF OBJECT_ID('[SA].[ETL_FlightDetail]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_FlightDetail];
+IF OBJECT_ID('[SA].[ETL_FlightOperation]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_FlightOperation];
 IF OBJECT_ID('[SA].[ETL_Item]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_Item];
-IF OBJECT_ID('[SA].[ETL_LoyaltyTransactionType]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_LoyaltyTransactionType];
-IF OBJECT_ID('[SA].[ETL_PointConversionRate]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_PointConversionRate];
 IF OBJECT_ID('[SA].[ETL_LoyaltyTier]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_LoyaltyTier];
-
-IF OBJECT_ID('[SA].[ETL_MaintenanceType]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_MaintenanceType];
+IF OBJECT_ID('[SA].[ETL_LoyaltyTransactionType]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_LoyaltyTransactionType];
+IF OBJECT_ID('[SA].[ETL_Main_ETL]', 'P') IS NOT NULL DROP PROCEDURE [SA].[Main_ETL];
+IF OBJECT_ID('[SA].[ETL_Passenger]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_Passenger];
+IF OBJECT_ID('[SA].[ETL_Payment]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_Payment];
+IF OBJECT_ID('[SA].[ETL_Person]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_Person];
+IF OBJECT_ID('[SA].[ETL_PointConversionRate]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_PointConversionRate];
+IF OBJECT_ID('[SA].[ETL_Points]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_Points];
+IF OBJECT_ID('[SA].[ETL_PointsTransaction]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_PointsTransaction];
+IF OBJECT_ID('[SA].[ETL_Reservation]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_Reservation];
+IF OBJECT_ID('[SA].[ETL_SeatDetail]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_SeatDetail];
+IF OBJECT_ID('[SA].[ETL_ServiceOffering]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_ServiceOffering];
+IF OBJECT_ID('[SA].[ETL_ServiceOfferingItem]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_ServiceOfferingItem];
 IF OBJECT_ID('[SA].[ETL_Technician]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_Technician];
-IF OBJECT_ID('[SA].[ETL_MaintenanceLocation]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_MaintenanceLocation];
-IF OBJECT_ID('[SA].[ETL_Part]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_Part];
-
-IF OBJECT_ID('[SA].[ETL_AirlineAirportService]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_AirlineAirportService];
-
-IF OBJECT_ID('[SA].[Main_ETL]', 'P') IS NOT NULL DROP PROCEDURE [SA].Main_ETL;
+IF OBJECT_ID('[SA].[ETL_TravelClass]', 'P') IS NOT NULL DROP PROCEDURE [SA].[ETL_TravelClass];
 
 DROP SCHEMA IF EXISTS [SA];
-
+GO
