118ad2b64a05156ff2f0e7fef3fd0b39adfe9238
Pouria Dianati <137393369+Pouriadia@users.noreply.github.com>
2025-06-27 14:02:35 +0330

    Track date dim again

---
+++ Datawarehouse/Dimensions/Initial_Date_Dim.sql
@@ -0,0 +1,14 @@
+CREATE OR ALTER PROCEDURE [DW].[Initial_Date_Dim]
+AS
+BEGIN
+	TRUNCATE TABLE [DW].[DimDate];
+	BULK INSERT [DW].[DimDate]
+	FROM 'D:\Uni\Current Semester\DB2\Project\data-warehouse\Files\Date1.CSV' --'F:\UNI\Term8\DB2\Project\data-warehouse\Files\Date1.CSV'
+	WITH 
+	(
+		FIELDTERMINATOR = ',',   
+		ROWTERMINATOR = '\n',    
+		FIRSTROW = 2            
+	)
+END
+GO
