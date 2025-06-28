14b6912be690851175580d42f4f3edc37ef0bf35
mhtcode <masihtanoursaz@gmail.com>
2025-06-29 00:49:41 +0330

    Remove NOT NULL constraints from Rate and Currency columns in DimPointConversionRate table

--- Datawarehouse/Dimensions/Create_PointConversionRate_Dim.sql
+++ Datawarehouse/Dimensions/Create_PointConversionRate_Dim.sql
@@ -1,8 +1,8 @@
 CREATE TABLE [DW].[DimPointConversionRate] (
     ConversionRateKey      INT IDENTITY(1,1) PRIMARY KEY,   -- Surrogate Key
     PointConversionRateID  INT NOT NULL,                    -- Business Key from SA
-    Rate                   DECIMAL(18,6) NOT NULL,
-    Currency               NVARCHAR(255) NOT NULL,
+    Rate                   DECIMAL(18,6),
+    Currency               NVARCHAR(255),
     EffectiveFrom          DATETIME NOT NULL,
     EffectiveTo            DATETIME NULL,
     IsCurrent              BIT 
