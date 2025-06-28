a440c49cab733f388019a58d7cd57fe2a8d46cbc
mhtcode <masihtanoursaz@gmail.com>
2025-06-28 20:52:02 +0330

    Technician dim updated

--- Datawarehouse/Dimensions/Create_Technician_Dim.sql
+++ Datawarehouse/Dimensions/Create_Technician_Dim.sql
@@ -1,10 +1,8 @@
 CREATE TABLE [DW].[DimTechnician] (
   [Technician_ID] integer PRIMARY KEY,
-  [Name] nvarchar(255),
-  [Certification_Level] nvarchar(255),
-  [Employment_Type] nvarchar(255),
-  [Active_Status] bit
-)
+  [PersonID] INT,
+  [Specialty] NVARCHAR(100)
+);
 GO
 
 CREATE NONCLUSTERED INDEX IX_DimTechnician_Certification_Level
