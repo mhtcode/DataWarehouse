9419eaa97e3984e15d9d099814821760d9e845de
mhtcode <masihtanoursaz@gmail.com>
2025-06-29 01:57:51 +0330

    Debug in naming

--- Datawarehouse/Dimensions/Initial_Date_Dim.sql
+++ Datawarehouse/Dimensions/Initial_Date_Dim.sql
@@ -3,7 +3,7 @@ AS
 BEGIN
 	TRUNCATE TABLE [DW].[DimDate];
 	BULK INSERT [DW].[DimDate]
-	FROM 'C:\Users\Hkr\Desktop\1403-2\DB 2\Project\data-warehouse\Files\Date1.CSV' --'D:\Uni\Current Semester\DB2\Project\data-warehouse\Files\Date1.CSV' --'F:\UNI\Term8\DB2\Project\data-warehouse\Files\Date1.CSV'
+	FROM 'F:\UNI\Term8\DB2\Project\data-warehouse\Files\Date1.CSV' --'D:\Uni\Current Semester\DB2\Project\data-warehouse\Files\Date1.CSV' --'F:\UNI\Term8\DB2\Project\data-warehouse\Files\Date1.CSV'
 	WITH 
 	(
 		FIELDTERMINATOR = ',',   
