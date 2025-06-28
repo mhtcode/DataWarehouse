b6bff16633bba96c4e0f1cc29e4d937c10f226c8
Pouria Dianati <137393369+Pouriadia@users.noreply.github.com>
2025-06-28 07:54:35 +0330

    Change varchar to nvarchar for Unicode support
    Updated all relevant columns in staging and dimension table creation scripts from varchar to nvarchar to support Unicode data. This change ensures proper storage of multilingual and special character data across all tables.

--- Datawarehouse/Dimensions/Create_AirlineAirportService_Dim.sql
+++ Datawarehouse/Dimensions/Create_AirlineAirportService_Dim.sql
@@ -1,9 +1,9 @@
 CREATE TABLE [DW].[DimAirlineAirportService] (
     [ServiceDimKey] INT NOT NULL PRIMARY KEY,
-    [ServiceTypeCode] VARCHAR(50) NOT NULL,
-    [FlightTypeCode] VARCHAR(50) NOT NULL,
-    [ServiceTypeName] VARCHAR(100) NOT NULL,
-    [FlightTypeName] VARCHAR(100) NOT NULL,
+    [ServiceTypeCode] NVARCHAR(50) NOT NULL,
+    [FlightTypeCode] NVARCHAR(50) NOT NULL,
+    [ServiceTypeName] NVARCHAR(100) NOT NULL,
+    [FlightTypeName] NVARCHAR(100) NOT NULL,
     [ContractStartDate] DATE NOT NULL,
     [ContractEndDate] DATE,
     [LandingFeeRate] DECIMAL(18,4),
