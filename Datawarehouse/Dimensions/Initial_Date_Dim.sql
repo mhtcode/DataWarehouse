b66d324cf46c56c8fdd0919129cd3aa34d222c8c
ardalan-di <ardalandelfardi@gmail.com>
2025-06-28 20:40:23 +0330

    date dim update

--- Datawarehouse/Dimensions/Initial_Date_Dim.sql
+++ Datawarehouse/Dimensions/Initial_Date_Dim.sql
@@ -3,12 +3,13 @@ AS
 BEGIN
 	TRUNCATE TABLE [DW].[DimDate];
 	BULK INSERT [DW].[DimDate]
-	FROM 'C:\Users\Hkr\Desktop\1403-2\DB 2\Project\data-warehouse\Files\Date1.CSV' --'D:\Uni\Current Semester\DB2\Project\data-warehouse\Files\Date1.CSV' --'F:\UNI\Term8\DB2\Project\data-warehouse\Files\Date1.CSV'
+	FROM 'C:\Users\Hkr\Desktop\1403-2\DB 2\Project\data-warehouse\Files\Date2.CSV' --'D:\Uni\Current Semester\DB2\Project\data-warehouse\Files\Date1.CSV' --'F:\UNI\Term8\DB2\Project\data-warehouse\Files\Date1.CSV'
 	WITH 
 	(
 		FIELDTERMINATOR = ',',   
 		ROWTERMINATOR = '\n',    
-		FIRSTROW = 2            
+		FIRSTROW = 2,
+		CODEPAGE = '65001'            
 	)
 END
 GO
